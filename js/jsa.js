c71088ef2b9cc63f182079b5594d31c833a4d96a
unknown <balakishiyevamina@gmail.com>
2018-05-30 01:46:47 +0400

    Ilk commit

---
+++ js/jsa.js
@@ -0,0 +1,38 @@
+
+
+
+function myFunction() {
+    		var x = document.getElementById('rezerv-contenta');
+    		var y = document.getElementById('rezervbtn');
+    		
+       		 x.style.display = 'block';
+       		 y.style.display = 'none';
+    			
+       			 
+				}
+
+
+function audiof() {
+	var x = document.getElementById('sounda');
+  x.play()
+}
+function audiof1() {
+  var x = document.getElementById('sounda1');
+  x.play()
+}
+function audiof2() {
+  var x = document.getElementById('sounda2');
+  x.play()
+}
+function audiof3() {
+  var x = document.getElementById('sounda3');
+  x.play()
+}
+function audiof4() {
+  var x = document.getElementById('sounda4');
+  x.play()
+}
+function audiof5() {
+  var x = document.getElementById('sounda5');
+  x.play()
+}
